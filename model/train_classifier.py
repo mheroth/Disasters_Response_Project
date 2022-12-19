a6384e509834a4209e0ecd12f8eefcc515961993
mheroth <95216325+mheroth@users.noreply.github.com>
2022-12-19 21:11:26 +0100

    Add files via upload
    Added doc strings + comments and convert all categories to binary

--- model/train_classifier.py
+++ model/train_classifier.py
@@ -17,29 +17,63 @@ from sklearn.ensemble import AdaBoostClassifier
 
 
 def load_data(database_filepath):
+    ''' load data from the database_filepath and gives the matrices X and Y for training and in addition the     category_names back 
+    
+    Parameters: 
+    database_filepath: filepath of the database
+    
+    Returns:
+    X: matrix of different input text which can used as training data
+    Y: matrix of corresponding category (one out of 36) which can used as training data
+    category_names: names of the 36 differentcategorys
+    
+    '''
+    # create engine
     engine = create_engine('sqlite:///' + database_filepath)
+    # read the sql table and save it in a pandas dataframe
     df = pd.read_sql_table('InsertTableName', 'sqlite:///' + database_filepath)
+    # convert 'related' value of 2  to binary 1
+    df.related.replace(2,1,inplace=True)
+    # extract input text for later training
     X = df.message.values
+    # extract corresponding categories
     Y = df[df.columns[4:]].values
+    # get names of categories out of dataframe columns
     category_names = list(df.columns[4:])
     return X, Y, category_names
 
 def tokenize(text):
-    # Todo: normalize case and remove punctuation
+    ''' receives a text and gives tokenized list of relevant words back
+    
+    Parameters:
+    text: text with punctuation, lower and upper cases as string
+    
+    Returns:
+    tokens: words in a list without punctuation and only lower cases and without english stopwords
+    
+    '''
+    # normalize case and remove punctuation
     text = re.sub(r"[^a-zA-Z0-9]", " ", text.lower())
     
-    # Todo: tokenize text
+    # tokenize text
     words = word_tokenize(text)
     
-    # Todo: lemmatize and remove stop words
+    # lemmatize and remove stop words
     words = [w for w in words if w not in stopwords.words("english")]
     tokens = [WordNetLemmatizer().lemmatize(w) for w in words]
     return tokens
 
 
 def build_model():
-    # Random Forest Classification
+    ''' build a pipeline with a CountVectorizer, TfidTransformer and a MultiOutputClassifier and create a         grid search object
+    
+    Returns:
+    cv: GridSearchCV object using the above mentioned pipeline and the in this function defined parameters
+    
+    '''
+    # Building pipeline with random forest classification
     pipeline = Pipeline([('vect', CountVectorizer()), ('tfidf', TfidfTransformer()), ('clf', MultiOutputClassifier(RandomForestClassifier(n_estimators = 10, n_jobs=-1)))])
+    # define parameters for grid search
     parameters =  {
         'clf__estimator__n_estimators': [10, 15],
         'clf__estimator__min_samples_split': [2, 4]}
@@ -49,14 +83,31 @@ def build_model():
     return cv
 
 def evaluate_model(model, X_test, Y_test, category_names):
+    ''' evaluate the model using the classification report and print them for each category name
+    
+    Parameters:
+    model: the trained model
+    X_text: test input dataset
+    Y_test: text output dataset
+    categroy_names: list of category names
+    '''
+    # calculate prediction for test input dataset
     y_pred_rf = model.predict(X_test)
-
+    # get classification report for every category name
     for i in range(36):
         print(category_names[i])
         print(classification_report(Y_test[:,i], y_pred_rf[:,i]))
 
 
 def save_model(model, model_filepath):
+    ''' save the model using pickle
+    
+    Parameters:
+    model: trained model
+    model_filepath: filepath where the model should be stored
+    
+    '''
+    # save model with pickle
     pickle.dump(model, open(model_filepath, 'wb'))
 
 
